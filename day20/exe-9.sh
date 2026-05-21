916f67d8f4e01c4c0fdf5b5a15b12778366fd52a
Sai-029 <nsai779988@gmail.com>
2026-05-21 21:40:15 +0530

    exercise on system monitoring commands

---
+++ day20/exe-9.sh
@@ -0,0 +1,21 @@
+uptime
+
+free
+free -h
+
+ps
+
+tcpdump --version
+sudo tcpdump
+sudo tcpdump -c 10
+
+w
+
+sudo yum install htop -y
+htop
+
+sudo apt install sysstat
+sudo yum install iostat -y
+iostat version
+iostat
+pidstat
