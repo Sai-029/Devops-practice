17e757700c4bfa0832ed89368a3bd43f8042a6ef
Sai-029 <nsai779988@gmail.com>
2026-05-22 21:57:29 +0530

    memory commands in linux

---
+++ day21/exe-10.sh
@@ -0,0 +1,6 @@
+tail -f app.log          # live-follow a log file (Ctrl+C to stop)
+wc -l file.txt           # count lines
+find . -name "*.py"      # find all python files
+find . -mtime -1         # files modified in last 1 day
+df -h                    # disk space
+free -h                  # RAM usage
